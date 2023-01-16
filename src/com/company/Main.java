5c37459857965f847e97a6b0edb1be919d9423d3
grzes <grzes1508@tlen.pl>
2023-01-16 19:26:21 +0100

    Zadanie 1

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,6 +2,24 @@ package com.company;
 
 public class Main {
     public static void main(String[] args) {
+        Animal dog = new Animal();
+        dog.species = "canis";
+        dog.weight = 10.0;
+        dog.name = "Szarik";
+
+        dog.takeForWalk();
+        dog.takeForWalk();
+        dog.takeForWalk();
+        dog.takeForWalk();
+        dog.takeForWalk();
+        dog.takeForWalk();
+        dog.takeForWalk();
+        dog.takeForWalk();
+        dog.takeForWalk();
+        dog.takeForWalk();
+        dog.takeForWalk();
+        dog.takeForWalk();
+        dog.feed();
 
     }
 }
